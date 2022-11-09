ef8018d09271913bf671055ea93fbd1322f73d6d
Krow10 <23462475+Krow10@users.noreply.github.com>
2022-11-08 19:16:41 -0500

    Add optional block extractor to block streaming function

--- pyfirehose/block_extractors/common.py
+++ pyfirehose/block_extractors/common.py
@@ -9,7 +9,7 @@ import os
 import os.path
 from collections.abc import Generator
 from contextlib import asynccontextmanager
-from typing import Callable, Dict, List, Sequence
+from typing import Callable, Dict, List, Optional, Sequence
 
 import grpc
 from google.protobuf.message import Message
@@ -67,17 +67,18 @@ def process_blocks(raw_blocks: Sequence[Message], block_processor: Callable[[cod
     for raw_block in raw_blocks:
         block = codec_pb2.Block()
         raw_block.Unpack(block)
-        for transaction in block_processor(block):
-            data.append(transaction)
+        for blob in block_processor(block):
+            data.append(blob)
 
     logging.info('Finished block processing, parsed %i rows of data [SUCCESS]', len(data))
 
     return data
 
-async def stream_blocks(start: int, end: int, secure_channel: grpc.aio.Channel,
-                        custom_include_expr: str, custom_exclude_expr: str) -> List[Message]:
+async def stream_blocks(start: int, end: int, secure_channel: grpc.aio.Channel, #pylint: disable=too-many-arguments
+                        custom_include_expr: str, custom_exclude_expr: str,
+                        block_processor: Optional[Callable[[codec_pb2.Block], Dict]] = None) -> List[Message] | List[Dict]:
     """
-    Return a subset of transactions for blocks between `start` and `end` using the provided filters.
+    Return raw blocks (or parsed data) for the subset period between `start` and `end` using the provided filters.
 
     Args:
         start:
@@ -90,11 +91,17 @@ async def stream_blocks(start: int, end: int, secure_channel: grpc.aio.Channel,
             A custom Firehose filter for tagging blocks as included.
         custom_exclude_expr:
             A custom Firehose filter for excluding blocks from the results.
+        block_processor:
+            Optional block processor function for directly parsing raw blocks.
+            The function will then return the parsed blocks instead.
+
+            Discouraged as it might cause congestion issues for the gRPC channel if the block processing takes too long.
+            Parsing the blocks *after* extraction allows for maximum throughput from the block stream.
 
     Returns:
-        A list of raw blocks (google.protobuf.any_pb2.Any objects).
+        A list of raw blocks (google.protobuf.any_pb2.Any objects) or parsed data if a block processor is supplied.
     """
-    raw_blocks = []
+    data = []
     current_block_number = start
     stub = bstream_pb2_grpc.BlockStreamV2Stub(secure_channel)
 
@@ -105,21 +112,43 @@ async def stream_blocks(start: int, end: int, secure_channel: grpc.aio.Channel,
     )
 
     try:
-        async for response in stub.Blocks(bstream_pb2.BlocksRequestV2( #pylint: disable=no-member
-            start_block_num=start,
-            stop_block_num=end,
-            fork_steps=['STEP_IRREVERSIBLE'],
-            include_filter_expr=custom_include_expr,
-            exclude_filter_expr=custom_exclude_expr
-        )):
-            logging.debug('[%s] Getting block number #%i (%i blocks remaining)...',
-                get_current_task_name(),
-                current_block_number,
-                end - current_block_number
-            )
+        # Duplicate code for moving invariant out of loop, preventing condition check on every block streamed
+        if block_processor:
+            async for response in stub.Blocks(bstream_pb2.BlocksRequestV2( #pylint: disable=no-member
+                start_block_num=start,
+                stop_block_num=end,
+                fork_steps=['STEP_IRREVERSIBLE'],
+                include_filter_expr=custom_include_expr,
+                exclude_filter_expr=custom_exclude_expr
+            )):
+                logging.debug('[%s] Getting block number #%i (%i blocks remaining)...',
+                    get_current_task_name(),
+                    current_block_number,
+                    end - current_block_number
+                )
 
-            current_block_number += 1
-            raw_blocks.append(response.block)
+                current_block_number += 1
+                block = codec_pb2.Block()
+                response.block.Unpack(block)
+
+                for blob in block_processor(block):
+                    data.append(blob)
+        else:
+            async for response in stub.Blocks(bstream_pb2.BlocksRequestV2( #pylint: disable=no-member
+                start_block_num=start,
+                stop_block_num=end,
+                fork_steps=['STEP_IRREVERSIBLE'],
+                include_filter_expr=custom_include_expr,
+                exclude_filter_expr=custom_exclude_expr
+            )):
+                logging.debug('[%s] Getting block number #%i (%i blocks remaining)...',
+                    get_current_task_name(),
+                    current_block_number,
+                    end - current_block_number
+                )
+
+                current_block_number += 1
+                data.append(response.block)
     except grpc.aio.AioRpcError as error:
         logging.error('[%s] Failed to process block number #%i: %s',
             get_current_task_name(),
@@ -130,4 +159,4 @@ async def stream_blocks(start: int, end: int, secure_channel: grpc.aio.Channel,
         raise BlockStreamException(start, end, current_block_number) from error
 
     logging.debug('[%s] Done !\n', get_current_task_name())
-    return raw_blocks
+    return data
