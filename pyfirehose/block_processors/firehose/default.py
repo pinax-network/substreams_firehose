1e4573aabef40c01091771be8d32e968272816b0
Krow10 <23462475+Krow10@users.noreply.github.com>
2022-11-24 05:23:17 -0500

    Add default processor for Firehose v2

---
+++ pyfirehose/block_processors/firehose/default.py
@@ -0,0 +1,16 @@
+"""
+SPDX-License-Identifier: MIT
+
+This module ...
+"""
+import logging
+
+from google.protobuf.message import Message
+
+from proto.generated.sf.antelope.type.v1 import type_pb2
+
+def eos_block_processor(raw_block: Message) -> dict:
+    block = type_pb2.Block()
+    raw_block.Unpack(block)
+
+    logging.debug(block)
