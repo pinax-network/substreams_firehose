3a3330ace586e7e08112333b0f1b0b5346f48b8d
Krow10 <23462475+Krow10@users.noreply.github.com>
2023-01-25 11:41:02 -0500

    Add rendered demo and utility script

--- scripts/make_demo.sh
+++ scripts/make_demo.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
+vhs < scripted_demo.tape
+ffmpeg -i demo.mp4 -i pinax_large.svg -filter_complex "[1]format=rgba,colorchannelmixer=aa=0.1[logo];[0][logo]overlay=(W-w)/2:(H-h)/2:format=auto,format=yuv420p" -c:a copy pyfirehose_eth_config_demo.mp4
+rm demo.mp4
 rm -rf jsonl/
 rm pyfirehose/config/firehose/eth.hjson
-vhs < scripted_demo.tape
-ffmpeg -i demo.mp4 -i ~/Pictures/pinax_large.svg -filter_complex "[1]format=rgba,colorchannelmixer=aa=0.1[logo];[0][logo]overlay=(W-w)/2:(H-h)/2:format=auto,format=yuv420p" -c:a copy pyfirehose_eth_config_demo.mp4
-rm demo.mp4
