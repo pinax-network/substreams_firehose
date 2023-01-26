e51bc9d730554320b4a6c0e7ce21bd43fd5961ec
Krow10 <23462475+Krow10@users.noreply.github.com>
2023-01-26 17:01:46 -0500

    [CONFIG UI] Handle loading main config file from different location than default

--- pyfirehose/config/ui/app.py
+++ pyfirehose/config/ui/app.py
@@ -5,10 +5,11 @@ Main app for the config GUI application.
 """
 
 import logging
+import os.path
 
 import hjson
 from npyscreen import NPSAppManaged
-from npyscreen import setTheme
+from npyscreen import notify_confirm, selectFile, setTheme
 from npyscreen.npysThemes import DefaultTheme
 
 from pyfirehose.config.ui.forms.main import MainForm
@@ -23,6 +24,11 @@ class ConfigApp(NPSAppManaged):
 
     See [npyscreen's documentation](https://npyscreen.readthedocs.io/application-objects.html#in-detail)
     for reference.
+
+    Attributes:
+        display_main_popup: Indicates if the main form should display a information message when loaded (holds the message itself in that case).
+        main_config: Dictionary representing the loaded main configuration file.
+        main_config_file: The filepath to the main configuration file.
     """
     STUB_CONFIG_CONFIRM_EDIT_FORM = 'STUB_CONFIG_CONFIRM_EDIT_FORM'
     STUB_CONFIG_ENPOINTS_FORM = 'STUB_CONFIG_ENDPOINTS_FORM'
@@ -37,16 +43,31 @@ class ConfigApp(NPSAppManaged):
         super().__init__()
 
         self.display_main_popup = None
-        self.main_config_file = 'pyfirehose/config.hjson' # TODO: Allow loading from other location if not found
-        with open(self.main_config_file, 'r', encoding='utf8') as config_file:
-            try:
-                self.main_config = hjson.load(config_file)
-            except hjson.HjsonDecodeError as error:
-                logging.exception('Error decoding main config file (%s): %s', self.main_config_file, error)
-                raise
+        self.main_config = None
+        self.main_config_file = 'pyfirehose/config.hjson'
 
     def onStart(self):
         setTheme(DefaultTheme)
+
+        if not os.path.isfile(self.main_config_file):
+            notify_confirm(
+                f'Main configuration file not found at default location "{self.main_config_file}".\n'
+                f'Please select a valid configuration file.',
+                title="Warning: file not found",
+                editw=1
+            )
+            self.main_config_file = selectFile(must_exist=True, confirm_if_exists=False)
+
+        try:
+            with open(self.main_config_file, 'r', encoding='utf8') as config_file:
+                try:
+                    self.main_config = hjson.load(config_file)
+                except hjson.HjsonDecodeError as error:
+                    logging.exception('Error decoding main config file (%s): %s', self.main_config_file, error)
+                    raise
+        except FileNotFoundError as error:
+            logging.error('Could not find main config file : %s', error)
+            raise RuntimeError from error
 
         self.addForm('MAIN', MainForm, name='PyFirehose config')
         self.addForm(self.STUB_CONFIG_ENPOINTS_FORM, StubConfigEndpointsForm, name='Stub config editing - Endpoints')
