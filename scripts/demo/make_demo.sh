f6cccd64b905107807b7ee02ea852d71bdf5f294
Krow10 <23462475+Krow10@users.noreply.github.com>
2023-02-03 17:35:22 -0500

    Move demo creation script to subfolder

---
+++ scripts/demo/make_demo.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+DEMO_FOLDER="scripts/demo"
+
+vhs < $DEMO_FOLDER/scripted_demo.tape
+ffmpeg -y -i $DEMO_FOLDER/demo.mp4 -i $DEMO_FOLDER/logo.svg -filter_complex "[1]format=rgba,colorchannelmixer=aa=0.1[logo];[0][logo]overlay=(W-w)/2:(H-h)/2:format=auto,format=yuv420p" -c:a copy $DEMO_FOLDER/pyfirehose_eth_config_demo.mp4
+rm $DEMO_FOLDER/demo.mp4
+rm -rf jsonl/
+rm pyfirehose/config/firehose/eth.hjson
--- scripts/make_demo.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-vhs < scripted_demo.tape
-ffmpeg -i demo.mp4 -i logo.svg -filter_complex "[1]format=rgba,colorchannelmixer=aa=0.1[logo];[0][logo]overlay=(W-w)/2:(H-h)/2:format=auto,format=yuv420p" -c:a copy ../pyfirehose_eth_config_demo.mp4
-rm demo.mp4
-rm -rf jsonl/
-rm pyfirehose/config/firehose/eth.hjson
