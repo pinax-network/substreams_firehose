6800deb32403be5ebd34a18bdbf443e59f7f330d
Krow10 <23462475+Krow10@users.noreply.github.com>
2023-02-16 20:24:49 -0500

    Update logo image path for docs

--- scripts/build_docs.sh
+++ scripts/build_docs.sh
@@ -3,6 +3,6 @@
 source .venv/bin/activate
 
 pdoc substreams_firehose -o docs/ -d google -t docs/ \
-	--logo https://raw.githubusercontent.com/pinax-network/substreams_firehose/prepare-pypi/docs/logo.png \
+	--logo https://raw.githubusercontent.com/pinax-network/substreams_firehose/main/docs/logo.png \
 	--logo-link https://github.com/pinax-network/substreams_firehose/ \
 	--footer-text "$(printf "%s (v%s) made with ♡ by %s for Pinax" $(hatch project metadata | jq -r .name,.version,.authors[0].name))"
